333531254e4965f1752920e8d57b61e969d1c410
Magty <1249093124@qq.com>
2020-05-14 19:35:02 +0800

    base提交

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import state from './state'
+import actions from './actions'
+import mutations from './mutations'
+import user from './modules/user'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  modules: {
+    user
+  }
+})
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/magty/' : '/'
+const resolve = dir => path.join(__dirname, dir)
+module.exports = {
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  productionSourceMap: false,
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
+}
